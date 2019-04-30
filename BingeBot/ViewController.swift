a6892605d137fa9c430dde81d491d480aee19f1f
Anna <piladzisi@gmail.com>
2019-04-30 10:54:56 +0300

    solidifying basic concepts

---
+++ BingeBot/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BingeBot
+//
+//  Created by anna.sibirtseva on 30/04/2019.
+//  Copyright © 2019 anna.sibirtseva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
